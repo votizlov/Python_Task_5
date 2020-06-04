7e59be52d250afa97c88efe3740cf6dfc01a4a40
votizlov <35692043+votizlov@users.noreply.github.com>
2020-06-04 17:54:28 +0300

    done

--- device.py
+++
@@ -1,7 +0,0 @@
-class Device:
-
-    def __init__(self, name, price, memo, rate):
-        self.name = name
-        self.price = price
-        self.memo = memo
-        self.rate = rate
--- iterator_main.py
+++ iterator_main.py
@@ -1,12 +1,5 @@
 from Abstract import Iterator
-from device import Device
 
-devices = []
-f = open("Input/1.txt", "r")
-f1 = f.readlines()
-for x in f1:
-    line = x.split()
-    devices.append(Device(line[0], int(line[1]), int(line[2]), int(line[3])))
 
 
 class ListIterator(Iterator):
@@ -42,62 +35,15 @@ class ListIterator(Iterator):
         return self._collection[self._cursor]
 
 
+import textwrap
 
-
-
-import random
-
-from student import Student
-
-
-def findCourceIndex(array, student):
-    for i in range(len(array)):
-        if array[i][0].cource == student.cource:
-            return i
-    return -1
-
-
-coursesWithStudents = []
-f = open("Test input/1.txt", "r")
+f = open("Input/1.txt", "r")
 f1 = f.readlines()
-for x in f1:
-    line = x.split()
-    stud = Student(line[0], int(line[1]), line[2], int(line[3]))
-    if findCourceIndex(coursesWithStudents, stud) == -1:
-        coursesWithStudents.append([stud])
-    else:
-        coursesWithStudents[findCourceIndex(coursesWithStudents, stud)].append(stud)
+textWith = 10
+iterator = ListIterator(f1, -1)
 
-iterator = ListIterator(coursesWithStudents, -1)
-
-
-def main():
-    output = ""
-    for cource in coursesWithStudents:
-        iterator.next()
-        bestMales = [Student("", "", "male", 0)]
-        bestFemales = [Student("", "", "female", 0)]
-        for stud in cource:
-            if stud.sex == "male":
-                if stud.avg > bestMales[0].avg:
-                    bestMales.clear()
-                    bestMales.append(stud)
-                elif stud.avg == bestMales[0].avg:
-                    bestMales.append(stud)
-            if stud.sex == "female":
-                if stud.avg > bestFemales[0].avg:
-                    bestFemales.clear()
-                    bestFemales.append(stud)
-                elif stud.avg == bestFemales[0].avg:
-                    bestFemales.append(stud)
-        if bestMales[0].avg > 0 and bestFemales[0].avg > 0:
-            output = output + "Курс: " + str(cource[0].cource) + " " + random.choice(
-                bestFemales).name + " " + random.choice(
-                bestMales).name + "\n"
-
-    f = open("Output/1.txt", "w+")
-    f.write(output)
-    print(output)
-
-
-main()
+f = open("Output/1.txt", "w+")
+iterator.first()
+for l in f1:
+    iterator.next()
+    f.write("\n" + "      " + textwrap.fill(iterator.current(), textWith))
